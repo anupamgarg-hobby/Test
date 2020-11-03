999220f448f2001170a0b344aa318e060598aec4
anupamgarg-hobby <anupamgarg2000@gmail.com>
2020-11-03 15:34:44 -0800

    test

--- src/Worker.java
+++ src/Worker.java
@@ -2,30 +2,30 @@ import java.util.HashMap;
 
 public class Worker {
     public Worker() {
-        data_ = new HashMap<>();
+        data = new HashMap<>();
     }
 
     public int getCount() {
-        return  data_.size();   
+        return  data.size();   
     }
 
     public void increment(String s) {
-        if (!data_.containsKey(s)) {
-            data_.put(s, new Counter());
+        if (!data.containsKey(s)) {
+            data.put(s, new Counter());
         }
-        data_.get(s).value++;
+        data.get(s).value++;
     }
 
     public void decrement(String s) {
         if (!data_.containsKey(s)) {
-            data_.put(s, new Counter());
+            data.put(s, new Counter());
         }
-        data_.get(s).value--;
+        data.get(s).value--;
     }
 
     public int get(String s) {
-        return data_.getOrDefault(s, new Counter()).value;
+        return data.getOrDefault(s, new Counter()).value;
     }
 
-    private HashMap<String, Counter> data_;
+    private HashMap<String, Counter> data;
 }
